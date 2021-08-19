3f38a745efbdfe0b9a9f50e697c37fb50fc36366
Jedforrest <jedforrest@gmail.com>
2021-08-19 17:01:38 +1000

    added base types

---
+++ src/basetypes.jl
@@ -0,0 +1,25 @@
+abstract type AbstractNode end
+
+struct Component <: AbstractNode
+    metamodel::Symbol
+    name::String
+    maxports::Int
+end
+Component(metamodel::Symbol, name::String=string(metamodel)) = Component(metamodel, name, 1)
+
+struct Junction <: AbstractNode
+    metamodel::Symbol
+end
+
+struct Bond <: lg.AbstractSimpleEdge{Integer}
+    src::AbstractNode
+    dst::AbstractNode
+end
+
+struct BondGraph <: lg.AbstractGraph{Integer}
+    metamodel::Symbol
+    name::String
+    nodes::Vector{AbstractNode}
+    bonds::Vector{Bond}
+end
+BondGraph() = BondGraph(:BG, "bg", AbstractNode[], Bond[])
