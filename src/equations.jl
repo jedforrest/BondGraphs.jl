1f3e8bb626ad2cb1332a80be13e8799edbd75910
Michael Pan <mpan92@gmail.com>
2021-11-16 13:04:02 +1100

    Revert "Added special symbol for 0 and 1 junctions"
    This reverts commit ab1fc8218ad795f0ef117d89f9b889f669db088c.

--- src/equations.jl
+++ src/equations.jl
@@ -51,11 +51,7 @@ ModelingToolkit.connect(::Type{MTKPort}, p1, p2) =
 
 
 _separator() = "ː"
-mtk_name(m) = Symbol(string(vartype(m))*_separator()*string(m.name))
-
-vartype(n) = type(n)
-vartype(n::EqualEffort) = :𝟎
-vartype(n::EqualFlow) = :𝟏
+mtk_name(m) = Symbol(string(type(m))*_separator()*string(m.name))
 
 function ModelingToolkit.ODESystem(n::AbstractNode)
     N = numports(n)
