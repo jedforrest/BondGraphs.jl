7b55ce710c065d4787184a2beef2d6e9ef1d77e1
Jedforrest <jedforrest@gmail.com>
2022-03-18 18:23:54 +1100

    updated default storage

--- src/basetypes/AbstractNode.jl
+++ src/basetypes/AbstractNode.jl
@@ -6,26 +6,22 @@ struct Component{N} <: AbstractNode
     name::Symbol
     freeports::MVector{N,Bool}
     vertex::RefValue{Int}
-    parameters::AbstractDict{Num,Any}
-    # parameters::Vector{Num}
-    states::AbstractDict{Num,Any}
-    # states::Vector{Num}
+    parameters::AbstractDict
+    states::AbstractDict
     equations::Vector{Equation}
-    # defaults::AbstractDict
-    function Component{N}(t, n, v::Int, p::Vector, x::Vector, eq::Vector, def::AbstractDict) where {N}
-        new(Symbol(t), Symbol(n), ones(MVector{N,Bool}), Ref(v), p, x, eq, def)
+    function Component{N}(t, n, v, p, x, eq) where {N}
+        new(Symbol(t), Symbol(n), ones(MVector{N,Bool}), Ref(v), p, x, eq)
     end
 end
 
-function Component(type, name = type; vertex::Int = 0, library = BondGraphs.DEFAULT_LIBRARY,
-    comp_dict = haskey(library, type) ? library[type] : Dict(),
-    numports::Int = haskey(comp_dict, :numports) ? comp_dict[:numports] : 1,
-    parameters = haskey(comp_dict, :parameters) ? collect(keys(comp_dict[:parameters])) : Num[],
-    states = haskey(comp_dict, :states) ? collect(keys(comp_dict[:states])) : Num[],
-    equations = haskey(comp_dict, :equations) ? comp_dict[:equations] : Equation[],
-    defaults = haskey(comp_dict, :defaults) ? copy(comp_dict[:defaults]) : Dict())
+function Component(type, name=type; vertex::Int=0, library=BondGraphs.DEFAULT_LIBRARY,
+    comp_dict=haskey(library, type) ? library[type] : Dict(),
+    numports::Int=haskey(comp_dict, :numports) ? comp_dict[:numports] : 1,
+    parameters=haskey(comp_dict, :parameters) ? comp_dict[:parameters] : Dict(),
+    states=haskey(comp_dict, :states) ? comp_dict[:states] : Dict(),
+    equations=haskey(comp_dict, :equations) ? comp_dict[:equations] : Equation[])
 
-    Component{numports}(type, name, vertex, parameters, states, equations, defaults)
+    Component{numports}(type, name, vertex, parameters, states, equations)
 end
 
 # Source-sensor
@@ -33,7 +29,7 @@ struct SourceSensor <: AbstractNode
     name::Symbol
     freeports::MVector{1,Bool}
     vertex::RefValue{Int}
-    SourceSensor(; name = :SS, v::Int = 0) = new(Symbol(name), ones(MVector{1,Bool}), Ref(v))
+    SourceSensor(; name=:SS, v::Int=0) = new(Symbol(name), ones(MVector{1,Bool}), Ref(v))
 end
 
 
@@ -45,7 +41,7 @@ struct EqualEffort <: Junction
     freeports::Vector{Bool}
     weights::Vector{Int}
     vertex::RefValue{Int}
-    EqualEffort(; name = Symbol("0"), v::Int = 0) = new(Symbol(name), [true], [0], Ref(v))
+    EqualEffort(; name=Symbol("0"), v::Int=0) = new(Symbol(name), [true], [0], Ref(v))
 end
 
 struct EqualFlow <: Junction
@@ -53,7 +49,7 @@ struct EqualFlow <: Junction
     freeports::Vector{Bool}
     weights::Vector{Int}
     vertex::RefValue{Int}
-    EqualFlow(; name = Symbol("1"), v::Int = 0) = new(Symbol(name), [true], [0], Ref(v))
+    EqualFlow(; name=Symbol("1"), v::Int=0) = new(Symbol(name), [true], [0], Ref(v))
 end
 
 
@@ -61,7 +57,7 @@ end
 # Type
 type(n::AbstractNode) = n.type
 type(j::Junction) = typeof(j)
-type(ss::SourceSensor) = :SS
+type(::SourceSensor) = :SS
 
 # Name
 name(n::AbstractNode) = n.name
@@ -92,12 +88,12 @@ vertex(n::AbstractNode) = n.vertex[]
 set_vertex!(n::AbstractNode, v::Int) = n.vertex[] = v
 
 # Parameters
-parameters(n::AbstractNode) = keys(n.parameters)
+parameters(n::AbstractNode) = collect(keys(n.parameters))
 parameters(::Junction) = Num[]
 parameters(::SourceSensor) = Num[]
 
 # State variables
-states(n::AbstractNode) = keys(n.states)
+states(n::AbstractNode) = collect(keys(n.states))
 states(::Junction) = Num[]
 states(::SourceSensor) = Num[]
 
@@ -107,33 +103,35 @@ equations(::Junction) = Equation[]
 equations(::SourceSensor) = Equation[]
 
 # Defaults
-# defaults(n::AbstractNode) = n.defaults
-# defaults(::Junction) = Dict{Num,Any}()
-# defaults(::SourceSensor) = Dict{Num,Any}()
+defaults(n::AbstractNode) = merge(n.parameters, n.states)
+defaults(::Junction) = Dict{Num,Any}()
+defaults(::SourceSensor) = Dict{Num,Any}()
 
 # Set and get default parameter values
 function get_parameter(n::AbstractNode, var)
     p, = @parameters $var
-    p in keys(defaults(n)) || error("Component does not have parameter $var")
-    defaults(n)[p]
+    string(p) in string.(parameters(n)) || error("Component does not have parameter $var")
+    n.parameters[p]
 end
 function set_parameter!(n::AbstractNode, var, val)
     p, = @parameters $var
-    p in keys(defaults(n)) || error("Component does not have parameter $var")
-    defaults(n)[p] = val
+    string(p) in string.(parameters(n)) || error("Component does not have parameter $var")
+    n.parameters[p] = val
 end
 
 # Set and getinitial conditions
 function get_initial_value(n::AbstractNode, var)
     _, x = @variables t, $var(t)
-    x in keys(defaults(n)) || error("Component does not have state variable $var")
-    defaults(n)[x]
+    string(x) in string.(states(n)) || error("Component does not have state variable $var")
+    n.states[x]
 end
 function set_initial_value!(n::AbstractNode, var, val)
     _, x = @variables t, $var(t)
-    x in keys(defaults(n)) || error("Component does not have state variable $var")
-    defaults(n)[x] = val
+    string(x) in string.(states(n)) || error("Component does not have state variable $var")
+    n.states[x] = val
 end
+
+# _check_if_var_exists(Vars, var::Num) = any(var - v == 0 for v in Vars)
 
 # BASE FUNCTIONS
 # This definition will need to expand when equations etc. are added
--- test/simulation_tests.jl
+++ test/simulation_tests.jl
@@ -43,17 +43,17 @@ end
     tspan = (0.0, 10.0)
     sol = simulate(bg, tspan)
     for t in [0.5, 1.0, 5.0, 10.0]
-        @test isapprox(sol(t)[1], f(t, 10, 2), atol = 1e-5)
+        @test isapprox(sol(t)[1], f(t, 10, 2), atol=1e-5)
     end
 
-    sol = simulate(bg, tspan; u0 = [5.0])
+    sol = simulate(bg, tspan; u0=[5.0])
     for t in [0.5, 1.0, 5.0, 10.0]
-        @test isapprox(sol(t)[1], f(t, 5, 2), atol = 1e-5)
+        @test isapprox(sol(t)[1], f(t, 5, 2), atol=1e-5)
     end
 
-    sol = simulate(bg, tspan; pmap = [1.0, 3.0])
+    sol = simulate(bg, tspan; pmap=[1.0, 3.0])
     for t in [0.5, 1.0, 5.0, 10.0]
-        @test isapprox(sol(t)[1], f(t, 10, 3), atol = 1e-5)
+        @test isapprox(sol(t)[1], f(t, 10, 3), atol=1e-5)
     end
 end
 
@@ -61,20 +61,20 @@ end
     r1 = Component(:R, :r1)
     r2 = Component(:R, :r2)
     c = Component(:C)
-    kcl = EqualFlow(name = :kcl)
+    kcl = EqualFlow(name=:kcl)
     bg = BondGraph(:RRC)
-    
+
     add_node!(bg, [c, r1, r2, kcl])
     connect!(bg, c, kcl)
     connect!(bg, kcl, r1)
     connect!(bg, kcl, r2)
-    
+
     R1 = 1.0
     R2 = 2.0
-    Req = R1+R2
+    Req = R1 + R2
     C = 3.0
-    τ = Req*C
-    
+    τ = Req * C
+
     set_parameter!(r1, :R, R1)
     set_parameter!(r2, :R, R2)
     set_parameter!(c, :C, C)
@@ -85,32 +85,43 @@ end
     tspan = (0.0, 10.0)
     sol = simulate(bg, tspan)
     for t in [0.5, 1.0, 5.0, 10.0]
-        @test isapprox(sol(t)[1], f(t, 10, Req*C), atol = 1e-5)
+        @test isapprox(sol(t)[1], f(t, 10, Req * C), atol=1e-5)
     end
 end
 
 @testset "π-filter" begin
-    Se = Component(:Se, :Pin); set_parameter!(Se, :e, 1)
+    Se = Component(:Se, :Pin)
+    set_parameter!(Se, :e, 1)
 
-    Pa = EqualEffort(name = :Pa)
-    fa = EqualFlow(name = :fa)
-    ca = Component(:C, :Ca); set_parameter!(ca, :C, 1); set_initial_value!(ca, :q, 1)
-    rpa = Component(:R, :Rpa); set_parameter!(rpa, :R, 1)
-    
-    Pb = EqualEffort(name = :Pb)
-    fb = EqualFlow(name = :fb)
-    cb = Component(:C, :Cb); set_parameter!(cb, :C, 1); set_initial_value!(cb, :q, 2)
-    rpb = Component(:R, :Rpb); set_parameter!(rpb, :R, 1)
-    
-    fs = EqualFlow(name = :fs)
-    l = Component(:I, :L); set_parameter!(l, :L, 1); set_initial_value!(l, :p, 1)
-    r = Component(:R, :Rs); set_parameter!(r, :R, 1)
-    
-    rl = Component(:R, :RL); set_parameter!(rl, :R, 1)
-    
+    Pa = EqualEffort(name=:Pa)
+    fa = EqualFlow(name=:fa)
+    ca = Component(:C, :Ca)
+    set_parameter!(ca, :C, 1)
+    set_initial_value!(ca, :q, 1)
+    rpa = Component(:R, :Rpa)
+    set_parameter!(rpa, :R, 1)
+
+    Pb = EqualEffort(name=:Pb)
+    fb = EqualFlow(name=:fb)
+    cb = Component(:C, :Cb)
+    set_parameter!(cb, :C, 1)
+    set_initial_value!(cb, :q, 2)
+    rpb = Component(:R, :Rpb)
+    set_parameter!(rpb, :R, 1)
+
+    fs = EqualFlow(name=:fs)
+    l = Component(:I, :L)
+    set_parameter!(l, :L, 1)
+    set_initial_value!(l, :p, 1)
+    r = Component(:R, :Rs)
+    set_parameter!(r, :R, 1)
+
+    rl = Component(:R, :RL)
+    set_parameter!(rl, :R, 1)
+
     bg = BondGraph(:π_filter)
     add_node!(bg, [Se, Pa, fa, ca, rpa, Pb, fb, cb, rpb, fs, l, r, rl])
-    
+
     connect!(bg, Se, Pa)
     connect!(bg, Pa, fa)
     connect!(bg, fa, ca)
@@ -123,52 +134,57 @@ end
     connect!(bg, fb, cb)
     connect!(bg, fb, rpb)
     connect!(bg, Pb, rl)
-    
-    tspan = (0,100.0)
-    sol = simulate(bg,tspan)
-    @test sol[1] == [1,2,1]
+
+    tspan = (0, 100.0)
+    sol = simulate(bg, tspan)
+    @test sol[1] == [1, 2, 1]
     @test isapprox(sol[end][1], 1.0, atol=1e-5)
     @test isapprox(sol[end][2], 0.5, atol=1e-5)
     @test isapprox(sol[end][3], 0.5, atol=1e-5)
 end
 
 @testset "Simulate modular BG" begin
-    r = Component(:R); set_parameter!(r, :R, 1)
-    l = Component(:I); set_parameter!(l, :L, 1); set_initial_value!(l, :p, 1)
-    c = Component(:C); set_parameter!(c, :C, 1); set_initial_value!(c, :q, 1)
-    kvl = EqualEffort(name = :kvl)
-    SS1 = SourceSensor(name = :SS1)
-    SS2 = SourceSensor(name = :SS2)
-    
+    r = Component(:R)
+    set_parameter!(r, :R, 1)
+    l = Component(:I)
+    set_parameter!(l, :L, 1)
+    set_initial_value!(l, :p, 1)
+    c = Component(:C)
+    set_parameter!(c, :C, 1)
+    set_initial_value!(c, :q, 1)
+    kvl = EqualEffort(name=:kvl)
+    SS1 = SourceSensor(name=:SS1)
+    SS2 = SourceSensor(name=:SS2)
+
     bg1 = BondGraph(:RC)
     add_node!(bg1, [r, c, kvl, SS1])
     connect!(bg1, r, kvl)
     connect!(bg1, c, kvl)
     connect!(bg1, SS1, kvl)
     bgn1 = BondGraphNode(bg1)
-    
+
     bg2 = BondGraph(:L)
     add_node!(bg2, [l, SS2])
     connect!(bg2, l, SS2)
     bgn2 = BondGraphNode(bg2)
-    
+
     bg = BondGraph()
     add_node!(bg, [bgn1, bgn2])
     connect!(bg, bgn1, bgn2)
-    
+
     constitutive_relations(bg)
-    
-    tspan = (0,10.0)
-    sol = simulate(bg,tspan)
-    
+
+    tspan = (0, 10.0)
+    sol = simulate(bg, tspan)
+
     τ = 2
-    ω = sqrt(3)/2
-    f(t,τ,ω) = exp(-t/τ)*[
-        cos(ω*t) - sqrt(3)*sin(ω*t),
-        cos(ω*t) + sqrt(3)*sin(ω*t)
+    ω = sqrt(3) / 2
+    f(t, τ, ω) = exp(-t / τ) * [
+        cos(ω * t) - sqrt(3) * sin(ω * t),
+        cos(ω * t) + sqrt(3) * sin(ω * t)
     ]
-    
+
     for t in [0.0, 0.5, 1.0, 5.0, 10.0]
-        @test isapprox(sol(t), f(t,τ,ω), atol = 1e-5)
+        @test isapprox(sol(t), f(t, τ, ω), atol=1e-5)
     end
 end
